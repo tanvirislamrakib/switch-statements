c6923737878cc4f0854f712a26c9681a0374547b
tanvirislamrakib <155340897+tanvirislamrakib@users.noreply.github.com>
2024-01-04 14:27:02 +0600

    Add files via upload

---
+++ switch.js
@@ -0,0 +1,48 @@
+var sum = 10;
+switch(sum){
+    case 10:
+        console.log("Hello World")
+        break;
+    case 5:
+        console.log("Now i am in 5")
+        break;
+    case 1:
+        console.log("Now i am in 1")
+        break;
+    default:
+         console.log("Ami tomake khuje pelam na")
+}
+
+
+
+var sum = 10;
+switch(sum){
+    case 10:
+        console.log("Hello World")
+        //break;
+        case 5:
+            console.log("Now i am in 5")
+            //break;
+        case 1:
+            console.log("Now i am in 1")
+            //break;
+        default:
+            console.log("Ami tomake khuje pelam na")
+}
+
+
+
+var sum = "Hello my gf";
+switch(sum){
+    case "hajhsdgf":
+        console.log("Hello World")
+        break;
+        case "Hello my gf":
+            console.log("Now i am in 5")
+            break;
+        case "ajhdsfyie":
+            console.log("Now i am in 1")
+            break;
+        default:
+            console.log("Ami tomake khuje pelam na")
+}
